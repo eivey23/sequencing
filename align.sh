5c82e5780dd437abce82c0b37e7b4db31b5422a6
Emily Ivey <emilyive@buffalo.edu>
2025-06-04 23:04:02 -0400

    add code to generate alignment

---
+++ align.sh
@@ -0,0 +1,12 @@
+source ./common_values.sh
+
+for BASE_FILE in "Exp1sh90KP3" "Exp1shCP3" "Exp4sh90KP3" "Exp4shCP3"; do
+    echo base file: $BASE_FILE
+    R1_FILE="../30-1189397265/00_fastq/${BASE_FILE}_R1_001.fastq.gz"
+    echo R1: $R1_FILE
+    R2_FILE="../30-1189397265/00_fastq/${BASE_FILE}_R2_001.fastq.gz"
+    echo R2: $R2_FILE
+
+    bowtie2 -x "$INDEX" -1 "$R1_FILE" -2 "$R2_FILE" -S "$BASE_FILE.sam"
+    echo
+done
