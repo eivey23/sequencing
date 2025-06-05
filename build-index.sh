08c40311440bdcc8cfcbacba448daa1bc051783e
Emily Ivey <emilyive@buffalo.edu>
2025-06-04 22:48:48 -0400

    Build index

--- build-index.sh
+++ build-index.sh
@@ -1,1 +1,4 @@
-bowtie2-build sars-cov2-wa1-2020.FASTA sars-cov2-wa1-2020
+INDEX="sars-cov2-wa1-2020-index/sars-cov2-wa1-2020"
+mkdir -p "$(dirname "$INDEX")"
+
+bowtie2-build sars-cov2-wa1-2020.FASTA $INDEX
