14eb4a998b78a18d10694e4532ebe93d3c08213c
Emily Ivey <emilyive@buffalo.edu>
2025-06-09 15:48:58 -0400

    build indexes 2 and 3 and update alignment to try all pairwise

--- align.sh
+++ align.sh
@@ -1,12 +1,17 @@
 source ./common_values.sh
 
-for BASE_FILE in "Exp1sh90KP3" "Exp1shCP3" "Exp4sh90KP3" "Exp4shCP3"; do
-    echo base file: $BASE_FILE
-    R1_FILE="../30-1189397265/00_fastq/${BASE_FILE}_R1_001.fastq.gz"
-    echo R1: $R1_FILE
-    R2_FILE="../30-1189397265/00_fastq/${BASE_FILE}_R2_001.fastq.gz"
-    echo R2: $R2_FILE
+mkdir -p alignment
 
-    bowtie2 -x "$INDEX2" -1 "$R1_FILE" -2 "$R2_FILE" -S "$BASE_FILE.sam"
-    echo
+for BASE_FILE in "Exp1sh90KP3" "Exp1shCP3" "Exp4sh90KP3" "Exp4shCP3"; do
+    for CURRENT_INDEX in "$INDEX2" "$INDEX3" "$INDEX"; do
+        echo base file: $BASE_FILE
+        echo current index: $CURRENT_INDEX
+        R1_FILE="../30-1189397265/00_fastq/${BASE_FILE}_R1_001.fastq.gz"
+        echo R1: $R1_FILE
+        R2_FILE="../30-1189397265/00_fastq/${BASE_FILE}_R2_001.fastq.gz"
+        echo R2: $R2_FILE
+
+        bowtie2 -x "$CURRENT_INDEX" -1 "$R1_FILE" -2 "$R2_FILE" -S "alignment/$(basename "$CURRENT_INDEX")---$BASE_FILE.sam"
+        echo
+    done
 done
--- build-index.sh
+++ build-index.sh
@@ -1,6 +1,8 @@
 source ./common_values.sh
 
 mkdir -p "$(dirname "$INDEX")"
+mkdir -p "$(dirname "$INDEX2")"
+mkdir -p "$(dirname "$INDEX3")"
 
 bowtie2-build sars-cov2-wa1-2020.FASTA $INDEX
 bowtie2-build WT-spike.FASTA $INDEX2
