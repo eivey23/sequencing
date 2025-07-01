2b0ea86514136a3efec7699ea966b4237baaa081
Emily Ivey <emilyive@buffalo.edu>
2025-07-01 18:12:03 -0400

    indexing

--- align.sh
+++ align.sh
@@ -42,7 +42,7 @@ for BASE_FILE in "${BASE_FILES[@]}"; do
         echo "R2: $R2_FILE"
 
         OUTPUT_SAM="$OUTPUT_BASE/sam/$BASE_FILE/$(basename "$CURRENT_INDEX")---$BASE_FILE.sam"
-        OUTPUT_BAM="$OUTPUT_BASE/bam/$BASE_FILE/$(basename "$CURRENT_INDEX")---$BASE_FILE.bam"
+        OUTPUT_BAM="$OUTPUT_BASE/bam/$BASE_FILE/$(basename "$CURRENT_INDEX")---$BASE_FILE.sorted.bam"
         OUTPUT_RESULTS="$OUTPUT_BASE/results/$BASE_FILE/$(basename "$CURRENT_INDEX")---$BASE_FILE.results"
 
         echo "Output SAM: $OUTPUT_SAM"
@@ -50,7 +50,9 @@ for BASE_FILE in "${BASE_FILES[@]}"; do
 
         mkdir -p "$OUTPUT_BASE/sam/$BASE_FILE" "$OUTPUT_BASE/bam/$BASE_FILE" "$OUTPUT_BASE/results/$BASE_FILE"
         # time bowtie2 --threads 8 -x "$CURRENT_INDEX" -1 "$R1_FILE" -2 "$R2_FILE" -S "$OUTPUT_SAM" 2>&1 | tee "$OUTPUT_RESULTS"
-        samtools view -Sb "$OUTPUT_SAM" | samtools sort - > "$OUTPUT_BAM"
+        samtools view --threads 8 -Sb "$OUTPUT_SAM" | samtools sort - > "$OUTPUT_BAM"
+        sleep 1
+        samtools index --threads 8 "$OUTPUT_BAM"
         echo
     done
 done
